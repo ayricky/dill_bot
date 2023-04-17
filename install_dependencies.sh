a229a765fa4b8ea608e3392460d24e65ba91c815
ayricky <marianoricardo97@gmail.com>
2023-04-16 22:43:28 -0400

    Add ffmeg to install_dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 apt-get update && \
-apt-get install -y libsndfile1 libportaudio2 portaudio19-dev && \
+apt-get install -y libsndfile1 libportaudio2 portaudio19-dev ffmpeg && \
 apt-get clean && \
 rm -rf /var/lib/apt/lists/*
