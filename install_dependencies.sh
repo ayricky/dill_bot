0018865cafee3a2770308d003c81e6075ecf8232
ayricky <61423971+ayricky@users.noreply.github.com>
2023-04-17 13:29:52 -0400

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-sudo apt-get update && \
-sudo apt-get install -y libsndfile1 libportaudio2 portaudio19-dev ffmpeg && \
-sudo apt-get clean
+apt-get update && \
+apt-get install -y libsndfile1 libportaudio2 portaudio19-dev ffmpeg && \
+apt-get clean && \
+rm -rf /var/lib/apt/lists/*
